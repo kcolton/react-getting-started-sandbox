71b431851223752352576788349093d203bae21d
Ken Colton <kcolton@gmail.com>
2018-08-04 00:29:06 -0400

    incrementValue prop onclick via inline function

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import "./styles.css";
 
 class Button extends React.Component {
   render() {
-    return <button onClick={this.props.onClickFunction}>+1</button>;
+    return (
+      <button
+        onClick={() => this.props.onClickFunction(this.props.incrementValue)}
+      >
+        +{this.props.incrementValue}
+      </button>
+    );
   }
 }
 
@@ -20,9 +26,9 @@ function Result(props) {
 class App extends React.Component {
   state = { counter: 0 };
 
-  incrementCounter = () => {
+  incrementCounter = incrementValue => {
     this.setState(prevState => ({
-      counter: prevState.counter + 1
+      counter: prevState.counter + incrementValue
     }));
   };
 
@@ -30,7 +36,10 @@ class App extends React.Component {
     return (
       // component must have single containing
       <div>
-        <Button onClickFunction={this.incrementCounter} />
+        <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={100} onClickFunction={this.incrementCounter} />
         <Result counter={this.state.counter} />
       </div>
     );
